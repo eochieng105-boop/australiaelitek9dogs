9fbe51a34d9cde0cad45ca64459364bf7cb2933e
Liege Writers <liegewriters6000@gmail.com>
2026-03-17 04:47:13 +0300

    initial commit

--- src/databaseHandler.ts
+++ src/databaseHandler.ts
@@ -4,11 +4,19 @@ type OrderRow = {
     OrderDate: number;
 };
 
+type Env = {
+    MY_DB: {
+        prepare: (query: string) => {
+            run: <T>() => Promise<T>;
+        };
+    };
+};
+
 export default {
-  async fetch(request, env) {
-    const result = await env.MY_DB.prepare(
-        "SELECT Id, CustomerName, OrderDate FROM [Order] ORDER BY ShippedDate DESC LIMIT 100",
-    ).run<OrderRow>();
-    return new Response(JSON.stringify(result));
-  }
+    async fetch(_: Request, env: Env) {
+        const result = await env.MY_DB.prepare(
+            "SELECT Id, CustomerName, OrderDate FROM [Order] ORDER BY ShippedDate DESC LIMIT 100"
+        ).run<OrderRow>();
+        return new Response(JSON.stringify(result));
+    },
 };
